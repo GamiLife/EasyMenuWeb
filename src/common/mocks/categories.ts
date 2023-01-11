d810f74e0f736b61440e140b0ce47889d994be74
Cesar Jesus Alberto Alvan Huarcaya <cesar.alvan@globant.com>
2023-01-11 13:58:02 -0500

    change

--- src/common/mocks/categories.ts
+++ src/common/mocks/categories.ts
@@ -1,21 +1,26 @@
 export const categories = [
   {
+    id: 1,
     text: 'Bebidas',
     icon: 'share' as const,
   },
   {
+    id: 2,
     text: 'Menu',
     icon: 'share' as const,
   },
   {
+    id: 3,
     text: 'Guarniciones',
     icon: 'share' as const,
   },
   {
+    id: 4,
     text: 'Pizzas',
     icon: 'share' as const,
   },
   {
+    id: 5,
     text: 'Marinos',
     icon: 'share' as const,
   },
---
+++ src/common/mocks/products.ts
@@ -0,0 +1,14 @@
+export const productsByCategory = [
+  {
+    categoryId: 1,
+    products: [{}],
+  },
+  {
+    categoryId: 2,
+    products: [{}],
+  },
+  {
+    categoryId: 3,
+    products: [{}],
+  },
+];
