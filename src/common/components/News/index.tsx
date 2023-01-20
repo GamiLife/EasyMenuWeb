8a981ed147ff2d314bc0f488ff27649f1321d75e
André Torres <andretorresjava@gmail.com>
2023-01-20 11:44:22 -0500

    Add styles to the Product component

--- src/common/components/News/index.tsx
+++ src/common/components/News/index.tsx
@@ -20,7 +20,6 @@ export const News = () => {
   useEffect(() => {
     async function newsFetch() {
       try{
-        // const { data } = await get(`news/companies/1?page=1&sizeByPage=3&byDate=${toISOString}&sort=[ "startDate", "ASC" ] , [ "id", "DESC" ]`);
         const { data } = await get(`news/companies/1?page=1&sizeByPage=3&byDate=2023-01-15T00:00:00Z&sort=[ "startDate", "ASC" ] , [ "id", "DESC" ]`);
         setNews(data);
       }catch(e){
